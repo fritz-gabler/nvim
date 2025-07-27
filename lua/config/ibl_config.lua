7cbe51f4741b3e2e39f33cc31d52dfba1af4e856
fritz-gabler <58051701+fritz-gabler@users.noreply.github.com>
2025-07-27 12:28:51 +0200

    change to new color pallet for scope lines

--- lua/config/ibl_config.lua
+++ lua/config/ibl_config.lua
@@ -1,3 +1,4 @@
+
 local highlight = {
   "IndBlue1", "IndBlue2", "IndBlue3", 
   "IndBlue4", "IndBlue5", "IndBlue6"
@@ -5,12 +6,12 @@ local highlight = {
 
 local hooks = require "ibl.hooks"
 hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-  vim.api.nvim_set_hl(0, "IndBlue1", { fg = "#7da4c2" }) -- lightest blue
-  vim.api.nvim_set_hl(0, "IndBlue2", { fg = "#6293b6" })
-  vim.api.nvim_set_hl(0, "IndBlue3", { fg = "#4d7ba6" })
-  vim.api.nvim_set_hl(0, "IndBlue4", { fg = "#3c618a" })
-  vim.api.nvim_set_hl(0, "IndBlue5", { fg = "#27416a" })
-  vim.api.nvim_set_hl(0, "IndBlue6", { fg = "#18253a" }) -- darkest blue
+  vim.api.nvim_set_hl(0, "IndBlue1", { fg = "#9290C3" }) -- lightest blue
+  vim.api.nvim_set_hl(0, "IndBlue2", { fg = "#535C91" })
+  vim.api.nvim_set_hl(0, "IndBlue3", { fg = "#1B1A55" })
+  vim.api.nvim_set_hl(0, "IndBlue4", { fg = "#070F2B" })
+  vim.api.nvim_set_hl(0, "IndBlue5", { fg = "#333446" })
+  vim.api.nvim_set_hl(0, "IndBlue6", { fg = "#393E46" }) -- darkest blue
 end)
 
 require("ibl").setup({
