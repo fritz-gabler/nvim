0ab4dcfc68f1e3d84d686e083595d23aa0bd82a1
fritz-gabler <58051701+fritz-gabler@users.noreply.github.com>
2025-07-27 12:29:00 +0200

    add new options to autopaors

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -2,12 +2,37 @@ return {
   {
     "windwp/nvim-autopairs",
     event = "InsertEnter",
-    config = true,
-    -- If you want to pass setup options, use opts = { ... }
-    -- Example:
-    -- opts = {
-    --   disable_filetype = { "TelescopePrompt", "vim" },
-    -- }
+    opts = {
+      map_char = {
+        all = "(",
+        tex = "{",
+      },
+      enable_check_bracket_line = false,
+      check_ts = true,
+      ts_config = {
+        lua = { "string", "source" },
+        javascript = { "string", "template_string" },
+        java = false,
+      },
+      disable_filetype = { "TelescopePrompt", "spectre_panel" },
+      ignored_next_char = string.gsub([[ [%w%%%'%[%"%.] ]], "%s+", ""),
+      enable_moveright = true,
+      disable_in_macro = false,
+      enable_afterquote = true,
+      map_bs = true,
+      map_c_w = false,
+      disable_in_visualblock = false,
+      fast_wrap = {
+        map = "<M-e>",
+        chars = { "{", "[", "(", '"', "'" },
+        pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+        offset = 0,
+        end_key = "$",
+        keys = "qwertyuiopzxcvbnmasdfghjkl",
+        check_comma = true,
+        highlight = "Search",
+        highlight_grey = "Comment",
+      },
+    }
   }
 }
-
