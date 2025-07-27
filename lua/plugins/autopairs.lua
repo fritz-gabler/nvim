c0961acf87b2a76ad34987dc5e1e0d2611431de7
fritz-gabler <58051701+fritz-gabler@users.noreply.github.com>
2025-07-27 12:06:23 +0200

    add autopairs

---
+++ lua/plugins/autopairs.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = true,
+    -- If you want to pass setup options, use opts = { ... }
+    -- Example:
+    -- opts = {
+    --   disable_filetype = { "TelescopePrompt", "vim" },
+    -- }
+  }
+}
+
