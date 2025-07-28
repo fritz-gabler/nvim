8436ed7df86335b3243b3905295f5c401f4681b5
fritz-gabler <58051701+fritz-gabler@users.noreply.github.com>
2025-07-28 14:04:37 +0200

    add new settings for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,3 +1,16 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    cmd = "Telescope", -- Optional: lazy-load on :Telescope
+    opts = {
+      defaults = {
+        file_ignore_patterns = { "%.o" },
+        winblend = 20, -- Add your winblend setting here!
+      },
+    },
+    config = function(_, opts)
+      require("telescope").setup(opts)
+    end,
+  },
 }
