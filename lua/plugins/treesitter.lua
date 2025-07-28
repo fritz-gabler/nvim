44d3f4e28b2bc06bbe53a39a958d2caf0a24f647
fritz-gabler <58051701+fritz-gabler@users.noreply.github.com>
2025-07-28 13:41:56 +0200

    add new languages to treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,7 +6,16 @@ return {
     build = ":TSUpdate",
     config = function()
       require("nvim-treesitter.configs").setup({
-        ensure_installed = { "lua", "python", "javascript" }, -- choose your languages
+        ensure_installed = {
+          "c",
+          "cpp",
+          "html",
+          "css",
+          "javascript",
+          "typescript",
+          "python",
+          "lua",
+        },
         highlight = { enable = true },
         indent = { enable = true },
       })
