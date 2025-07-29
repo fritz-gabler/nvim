3f8b3949433deef98f597ae6f04408ca83a1da02
fritz-gabler <58051701+fritz-gabler@users.noreply.github.com>
2025-07-29 18:02:38 +0200

    add better telescope settings a okayisch locking ui

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -13,4 +13,19 @@ return {
       require("telescope").setup(opts)
     end,
   },
+  {
+    'nvim-telescope/telescope-ui-select.nvim',
+    config = function()
+      require("telescope").setup {
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown {
+              -- even more opts
+            }
+          }
+        }
+      }
+      require("telescope").load_extension("ui-select")
+    end
+  },
 }
