fa205a8105fe490d2e5dc55a1a0cb79300a830cc
fritz-gabler <58051701+fritz-gabler@users.noreply.github.com>
2025-07-29 18:00:53 +0200

    add harpoon for faster file acces

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -57,9 +57,35 @@ function CopyWithoutLosing()
     vim.api.nvim_buf_set_lines(0, vim.fn.line('.') - 1, vim.fn.line('.'), false, { saved_line })
 end
 
+--Harpoon 
+vim.keymap.set("n", "<C-e>", function ()
+  require("harpoon.ui").toggle_quick_menu()
+end)
 
+vim.keymap.set("n", "<leader>a", function ()
+  require("harpoon.mark").add_file()
+end)
 
+vim.keymap.set("n", "<leader>n", function ()
+  require("harpoon.ui").nav_next()
+end)
 
+vim.keymap.set("n", "<leader>n", function ()
+  require("harpoon.ui").nav_prev()
+end)
 
+vim.keymap.set("n", "<C-j>", function ()
+  require("harpoon.ui").nav_file(1)
+end)
 
+vim.keymap.set("n", "<C-l>", function ()
+  require("harpoon.ui").nav_file(2)
+end)
 
+vim.keymap.set("n", "<C-o>", function ()
+  require("harpoon.ui").nav_file(3)
+end)
+
+vim.keymap.set("n", "<C-m>", function ()
+  require("harpoon.ui").nav_file(4)
+end)
