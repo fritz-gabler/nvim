4d606836ec8a71fd1160b577ad40c83c31e6c89b
fritz-gabler <58051701+fritz-gabler@users.noreply.github.com>
2025-07-29 18:05:17 +0200

    add language server settings and plugins

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    opts = {}
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "ts_ls" }
+       })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities
+      })
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities
+      })
+      lspconfig.ccls.setup({
+        capabilities = capabilities
+      })
+    end
+  }
+}
