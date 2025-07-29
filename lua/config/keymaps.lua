a9a55517d5686eb70fbad85f219a4bf860d9d27b
fritz-gabler <58051701+fritz-gabler@users.noreply.github.com>
2025-07-29 18:06:00 +0200

    add keymap to go back to files more easy

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,8 +2,12 @@
 vim.g.mapleader = " "
 
 ----------ALL-LEADER-----------------------
-vim.keymap.set("n", "<leader>pv", function() vim.cmd("Ex") end)
-vim.keymap.set("n", "<leader>h", function() vim.cmd("Std") end)
+vim.keymap.set("n", "<leader>pv", function()
+	vim.cmd("Ex")
+end)
+vim.keymap.set("n", "<leader>h", function()
+	vim.cmd("Std")
+end)
 vim.keymap.set("n", "<leader>l", ":")
 
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
